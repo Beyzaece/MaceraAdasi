d2a2132e6129c1c9c713323c5ea6b7c68eb4741e
Beyzaece <beyzaeced@gmail.com>
2025-06-01 01:36:44 +0300

    Gereksiz  dosyalar silindi;sadece kod ve sahne dosyaları bırakıldı

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -3,13 +3,44 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadAvatarScene()
+    public void LoadmalzemeScene()
     {
-        SceneManager.LoadScene("AvatarOynat");
+        SceneManager.LoadScene("secimmalzeme");
     }
 
     public void LoadWorldScene()
     {
         SceneManager.LoadScene("DunyaKesif");
+    }
+
+    public void LoadARScene()
+    {
+        SceneManager.LoadScene("ar");
+    }
+    public void LoadavatarolusturmaScene()
+    {
+        SceneManager.LoadScene("avataroluþturma");
+
+    }
+    public void LoadYemekScene()
+    {
+        SceneManager.LoadScene("KültürelYemek");
+
+    }
+
+    public void LoadmAvatarOynatScene()
+    {
+        SceneManager.LoadScene("AvatarOynat");
+    }
+
+    public void Loadabigben()
+    {
+        SceneManager.LoadScene("arbigben");
+
+    }
+    public void LoadgirisScene()
+    {
+        SceneManager.LoadScene("Girissahnesi");
+
     }
 }
